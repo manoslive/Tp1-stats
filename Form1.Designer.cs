41948330ff529b79d0442368e4b84ef73dd7a20f
manoslive <ebeloin@hotmail.com>
2015-01-26 11:29:38 -0500

    Form building 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.DGV_Fichier = new System.Windows.Forms.DataGridView();
+            this.DGV_Echantillon = new System.Windows.Forms.DataGridView();
+            this.GB_Methodes = new System.Windows.Forms.GroupBox();
+            this.GB_Fichier = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.BTN_Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Fichier)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Echantillon)).BeginInit();
+            this.GB_Methodes.SuspendLayout();
+            this.GB_Fichier.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(36, 45);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(126, 24);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(36, 75);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 24);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // DGV_Fichier
+            // 
+            this.DGV_Fichier.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_Fichier.Location = new System.Drawing.Point(28, 215);
+            this.DGV_Fichier.Name = "DGV_Fichier";
+            this.DGV_Fichier.RowTemplate.Height = 28;
+            this.DGV_Fichier.Size = new System.Drawing.Size(226, 372);
+            this.DGV_Fichier.TabIndex = 3;
+            // 
+            // DGV_Echantillon
+            // 
+            this.DGV_Echantillon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_Echantillon.Location = new System.Drawing.Point(344, 215);
+            this.DGV_Echantillon.Name = "DGV_Echantillon";
+            this.DGV_Echantillon.Size = new System.Drawing.Size(578, 372);
+            this.DGV_Echantillon.TabIndex = 3;
+            // 
+            // GB_Methodes
+            // 
+            this.GB_Methodes.Controls.Add(this.radioButton2);
+            this.GB_Methodes.Controls.Add(this.radioButton1);
+            this.GB_Methodes.Location = new System.Drawing.Point(544, 22);
+            this.GB_Methodes.Name = "GB_Methodes";
+            this.GB_Methodes.Size = new System.Drawing.Size(368, 178);
+            this.GB_Methodes.TabIndex = 4;
+            this.GB_Methodes.TabStop = false;
+            this.GB_Methodes.Text = "Méthodes d\'échantillonnage";
+            // 
+            // GB_Fichier
+            // 
+            this.GB_Fichier.Controls.Add(this.textBox3);
+            this.GB_Fichier.Controls.Add(this.textBox2);
+            this.GB_Fichier.Controls.Add(this.label1);
+            this.GB_Fichier.Controls.Add(this.label3);
+            this.GB_Fichier.Controls.Add(this.label2);
+            this.GB_Fichier.Controls.Add(this.textBox1);
+            this.GB_Fichier.Location = new System.Drawing.Point(28, 12);
+            this.GB_Fichier.Name = "GB_Fichier";
+            this.GB_Fichier.Size = new System.Drawing.Size(368, 178);
+            this.GB_Fichier.TabIndex = 4;
+            this.GB_Fichier.TabStop = false;
+            this.GB_Fichier.Text = "Information sur l\'échantillon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 99);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 1;
+            // 
+            // BTN_Save
+            // 
+            this.BTN_Save.Location = new System.Drawing.Point(813, 610);
+            this.BTN_Save.Name = "BTN_Save";
+            this.BTN_Save.Size = new System.Drawing.Size(109, 39);
+            this.BTN_Save.TabIndex = 5;
+            this.BTN_Save.Text = "Sauvegarder";
+            this.BTN_Save.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 661);
+            this.Controls.Add(this.BTN_Save);
+            this.Controls.Add(this.GB_Fichier);
+            this.Controls.Add(this.GB_Methodes);
+            this.Controls.Add(this.DGV_Echantillon);
+            this.Controls.Add(this.DGV_Fichier);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Fichier)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Echantillon)).EndInit();
+            this.GB_Methodes.ResumeLayout(false);
+            this.GB_Methodes.PerformLayout();
+            this.GB_Fichier.ResumeLayout(false);
+            this.GB_Fichier.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.DataGridView DGV_Fichier;
+        private System.Windows.Forms.DataGridView DGV_Echantillon;
+        private System.Windows.Forms.GroupBox GB_Methodes;
+        private System.Windows.Forms.GroupBox GB_Fichier;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BTN_Save;
     }
 }
 
